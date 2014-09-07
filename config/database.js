db6b0c92217f2bd45efef89e44360fc3cd98163f
Alex de Beer <alexdebeer@gmail.com>
2014-09-07 14:42:48 +0200

    DB server

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'localhost:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://dmkludde:halftwee@kahana.mongohq.com:10044/app29324067' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
